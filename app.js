ddd4aceb8399c811ffcccc9e33998569a7673bc3
codeeblacc <dycode999@gmail.com>
2022-09-05 15:45:15 +0100

    final build

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const formOffset = document.querySelector('.form-offset');
 const form = document.querySelector('form');
 const submitBtn = document.querySelector('#submit');
 const body = document.querySelector('body');
+const preloader = document.querySelector('.preloader');
 
 feather.replace();
 let menuOpen = false;
@@ -20,14 +21,12 @@ const closeMenu = () => {
 }
 
 const validateForm = () => {
-  if (form.name.value === "" || form.owner.value === "" || form.foundingYear.value === "") {
+  if (form.name.value === "" || form.location.value === "") {
     return false;
-  } else {
-    return true;
-  }
+  } else { return true; }
 }
 
-document.querySelectorAll("input").forEach((input) => {
+function validateInput(input) {
   input.addEventListener("keyup", (e) => {
     let validated = validateForm();
 
@@ -38,7 +37,11 @@ document.querySelectorAll("input").forEach((input) => {
       actionCon.removeAttribute("style");
       processCon.removeAttribute("style");
     }
-  })
+  });
+}
+
+document.querySelectorAll("input").forEach((input) => {
+  validateInput(input);
 });
 
 processBtn.addEventListener("click", () => {
@@ -49,7 +52,19 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   processBtn.classList.add("focus");
   body.style.pointerEvents = 'none';
-})
+
+  db.collection('jobs').add({
+    'name': form.name.value,
+    'location': form.location.value,
+  }).then((response) => {
+    form.reset();
+    processBtn.classList.remove("focus");
+    body.removeAttribute("style");
+    actionCon.removeAttribute("style");
+    processCon.removeAttribute("style");
+    closeMenu();
+  });
+});
 
 actionBtn.addEventListener("click", () => {
   if (!menuOpen) {
@@ -73,7 +88,7 @@ function renderBrand(doc) {
   let divAction = document.createElement('div');
   let img = document.createElement('img');
 
-  headerP.textContent = `${doc.data().owner}, ${doc.data().foundingYear}`;
+  headerP.textContent = `${doc.data().location}`;
   bodyP.textContent = doc.data().name;
 
   headerP.classList.add("small-text", "smoke-text", "ln");
@@ -92,10 +107,44 @@ function renderBrand(doc) {
 
 
   brandList.appendChild(li);
+
+  divAction.addEventListener('click', (e) => {
+    e.stopPropagation(); //optional
+    let id = divAction.parentElement.getAttribute('data-id');
+    db.collection('jobs').doc(id).delete();
+
+    if (brandList.children.length == 1) {
+      setTimeout(() => {
+        document.querySelector('.no-content').style.display = "flex";
+      }, 1000);
+    }
+  });
 }
 
-db.collection('brands').get().then((snapshot) => {
-  snapshot.docs.forEach(doc => {
-    renderBrand(doc);
-  });
-});
+// db.collection('jobs').orderBy('location').get().then((snapshot) => {
+//   snapshot.docs.forEach(doc => {
+//     renderBrand(doc);
+//   });
+// });
+
+
+db.collection('jobs').orderBy('location').onSnapshot((snapshot) => {
+  let shots = snapshot.docChanges();
+  preloader.remove();
+
+  if (shots.length == 0) {
+    document.querySelector('.no-content').style.display = "flex";
+  } else {
+    shots.forEach((shot) => {
+      if (shot.type == "added") {
+        renderBrand(shot.doc);
+        document.querySelector('.no-content').removeAttribute('style');
+      } else if (shot.type == "removed") {
+        let li = document.querySelector(`[data-id="${shot.doc.id}"]`);
+        li.classList.remove('animate__zoomInDown');
+        li.classList.add('animate__zoomOutDown');
+        setTimeout(() => brandList.removeChild(li), 500);
+      }
+    });
+  }
+})
